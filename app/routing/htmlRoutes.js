60deeeb9445005eca688d0de554944a110f5d187
Jinnie24 <djinnie24@gmail.com>
2018-08-15 14:13:32 -0400

    Screenshots added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,8 @@
+var express = require("express");
 var path = require("path");
 
 module.exports = function(app){
+    app.use(express.static(__dirname + 'public'));
     app.get("/home", function(req, res) {
         res.sendFile(path.join(__dirname, "../public", "home.html"));
       });
